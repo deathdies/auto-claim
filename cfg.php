a90d978ff28ef4e37aa7aad199b4203dc3ab2604
deathdies <30880522+deathdies@users.noreply.github.com>
2020-05-31 17:32:46 +0700

    Add files via upload

---
+++ cfg.php
@@ -0,0 +1,5 @@
+<?php
+$address = "EC-UserId-85846";
+$ua = array(
+"user-agent: Mozilla/5.0 (Linux; Android 10; SM-G920F) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Mobile Safari/537.36"
+);
